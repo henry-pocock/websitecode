42f090ba3eb5e4b548636457ef879ac6e6debf19
henry-pocock <henry@thistlecottage.co.uk>
2021-06-16 14:12:46 +0100

    Node stuff added

--- postForm.js
+++ postForm.js
@@ -1,17 +1,36 @@
-function checkEnter() {
-    document.getElementById("pinNo")
-    .addEventListener('keyup', function(event) {
-        if (event.code === 'Enter') {
-            event.preventDefault();
-            document.querySelector('form').submit();
-        }
-    });
-}
 
-function userPin() {
-    var x = document.getElementById("pinNo");
+function userPin(pin) {
     var text = "231203";
-    if (x == text) {
-        window.location.assign('https://youtube.com');
+    console.log(pin);
+    if (pin == text) {
+        //window.location.assign('https://youtube.com');
     }
 }
+
+function postData() {
+    fetch(window.location.href, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({pin: document.getElementById("pinNo").value}),
+    }).then(raw => raw.json())
+      .then(json => {
+          const data = JSON.parse(json);
+          console.log(data);
+          if (data.pin === -1) {
+              window.location.href = window.location.origin + `/nextpage?id=${data.id}`;
+          }
+      })
+      .catch(e => {});
+}
+
+/*document.getElementById("pinNo")
+    .addEventListener('keyup', function(event) {
+        if (event.code === "Enter") {
+            pin = document.getElementById('pinNo').value;
+            console.log(pin);
+            userPin(pin);
+        }
+    });
+*/
