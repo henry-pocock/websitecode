1589317b983cd101a457bf8d2f56bda069c847b3
henry-pocock <henry@thistlecottage.co.uk>
2021-06-16 11:48:16 +0100

    First addition of JS

---
+++ postForm.js
@@ -0,0 +1,8 @@
+document.getElementById('pin')
+    .addEventListener('keyup', function(event) {
+        if (event.code === 'Enter')
+        {
+            event.preventDefault();
+            document.querySelector('form').submit();
+        }
+    });
