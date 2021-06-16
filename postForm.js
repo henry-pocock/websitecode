bdbeeefdae79ce9345038ed518de10d31940f19c
henry-pocock <henry@thistlecottage.co.uk>
2021-06-16 12:14:47 +0100

    12:14

--- postForm.js
+++ postForm.js
@@ -1,10 +1,17 @@
-document.getElementById('pin')
+function checkEnter() {
+    document.getElementById("pinNo")
     .addEventListener('keyup', function(event) {
-        if (event.code === 'Enter')
-        {
-            var x = document.forms[0];
-            if (x == "231203") {
-                window.location.assign('https://youtube.com');
-            }
+        if (event.code === 'Enter') {
+            event.preventDefault();
+            document.querySelector('form').submit();
         }
     });
+}
+
+function userPin() {
+    var x = document.getElementById("pinNo");
+    var text = "231203";
+    if (x == text) {
+        window.location.assign('https://youtube.com');
+    }
+}
